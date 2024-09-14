ac73e95bbbfe639bfa90c2edfebe8ff548504906
rohananand2023F <rohananand@vt.edu>
2024-09-14 17:00:36 -0400

    made combability improved for most sites

--- content.js
+++ content.js
@@ -1,71 +1,66 @@
 // content.js
 
-let recipe;
 
-// Function to find and log the <script type="application/ld+json"> tag
 function findLdJsonScript() {
 
     let ingredients;
 
-    console.log('Running findLdJsonScript function...');
     const ldJsonScripts = document.querySelectorAll('script[type="application/ld+json"]');
+    
     ldJsonScripts.forEach(script => {
 
         const jsonContent = JSON.parse(script.textContent);
         console.log('JSON content:', jsonContent);
+
+        // if json content is a array
         if (Array.isArray(jsonContent)) {
             jsonContent.forEach(content => {
-                // check if @type is an array
+                // if content['@type']  is an array
                 if (Array.isArray(content['@type'])) {
+                    console.log("array of types");
+                    
                     content['@type'].forEach(type => {
                         if (type === 'Recipe') {
-                            ingredients = content.recipeIngredient;
+                            ingredients = content['recipeIngredient'];
                         }
                     });
                 }
                 if (content['@type'] === 'Recipe') {
-                    recipe = content.recipeIngredient;
+                    console.log("type is recipe");
+                    ingredients = content['recipeIngredient'];
                 }
             });
-        }
-        else {
-            if (jsonContent['@graph']) {
-                console.log('Graph:', jsonContent['@graph']);
-                jsonContent['@graph'].forEach(graph => {
-                    if (Array.isArray(content['@type'])) {
-                        content['@type'].forEach(type => {
-                            if (type === 'Recipe') {
-                                ingredients = content.recipeIngredient;
-                            }
-                        });
-                    }
-                    if (graph['@type'] === 'Recipe') {
-                        ingredients = graph.recipeIngredient;
+        } else {
+            console.log('Single content');
+            if (jsonContent['@type'] === 'Recipe') {
+                ingredients = jsonContent['recipeIngredient'];   
+            }
+            else if (jsonContent['@graph']) {
+                jsonContent['@graph'].forEach(content => {
+                    if (content['@type'] === 'Recipe') {
+                        ingredients = content['recipeIngredient'];
                     }
                 });
             }
-            else if (jsonContent['@type'] === 'Recipe') {
-                ingredients = jsonContent.recipeIngredient;
-            }
         }
-
-
-        // check if the json content is a array
- 
-
+        
     });
 
-    if (recipe) {
 
-        chrome.storage.local.set({ currentIngredients: ingredients }, function() {
-            console.log('Ingredients saved to local storage');
+    if (ingredients) {
+        
+        chrome.storage.local.set({currentIngredients: ingredients}, () => {
+            console.log('Ingredients set to storage');
         });
+        
+    } else {
+        console.log('No ingredients found');
+        
     }
-
+    
 }
 
-// Run the function when the DOM is fully loaded
+
+
 
 findLdJsonScript();
-console.log('Content script loaded');
-
