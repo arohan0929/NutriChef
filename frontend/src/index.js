f86b13878ab36db5b2069194e8ec04b25108fbc5
rohananand2023F <rohananand@vt.edu>
2024-09-13 23:40:06 -0400

    initial commit
    initial commit setting up the manifest and svelte files

---
+++ frontend/src/index.js
@@ -0,0 +1,8 @@
+import './app.css'
+import App from './pages/index.svelte'
+
+const app = new App({
+  target: document.getElementById('app'),
+})
+
+export default app
---
+++ frontend/vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        options: resolve(__dirname, 'options.html'),
+        popup: resolve(__dirname, 'popup.html'),
+      }
+    }
+  },
+  base: './',
+})
